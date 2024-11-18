d2435966c73254ec11de5ccd924508d432c26fdf
Will Weidler <weidlerwill@gmail.com>
2024-11-18 13:19:53 -0600

    Auto stash before merge of "main" and "origin/main"

--- DigginDeep.Web/obj/Debug/net8.0/DigginDeep.Web.AssemblyInfo.cs
+++ DigginDeep.Web/obj/Debug/net8.0/DigginDeep.Web.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigginDeep.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3788a6529d4da27d19cbe2041a1340ba9f3e3239")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06bcdeefe4368ec5cae93d0ff342d474e8c5fd3")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("DigginDeep.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigginDeep.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
